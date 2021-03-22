e6cc2a0ecba190bcf3ed7b2bceeffb31490c0dad
Mike D <mikedaug@gmail.com>
2021-03-22 11:28:51 -0500

    Added main function. All tests passing

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
+# print Hello, World to the screen
 
 
 import argparse
 
-def main():
+
+def get_args():
 	
 	parser = argparse.ArgumentParser(description='Say hello')
 	parser.add_argument('-n', '--name', metavar='name', 
 						default='World', help='Name to greet')
-	args = parser.parse_args()
-	
-	# print Hello, World to the screen
+	return parser.parse_args()
+
+
+def main():
+	args = get_args()
 	print("Hello, " + args.name + "!")
 
 
