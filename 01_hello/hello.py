45b5532b6b77967d6206b4c861ed2ef62ad85776
Mike D <mikedaug@gmail.com>
2021-03-22 18:14:55 -0500

    exercise complete

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,22 +1,29 @@
 #!/usr/bin/env python3
-# print Hello, World to the screen
+"""
+Copyright:   Michael Daugherty
+Last update: 03/22/2021	11:40:26 AM
+Purpose:     Say hello
+"""
 
 
 import argparse
 
 
 def get_args():
-	
-	parser = argparse.ArgumentParser(description='Say hello')
-	parser.add_argument('-n', '--name', metavar='name', 
-						default='World', help='Name to greet')
-	return parser.parse_args()
+    """ Get command-line arguments """
+
+    parser = argparse.ArgumentParser(description='Say hello')
+    parser.add_argument('-n', '--name', metavar='name', default='World',
+                        help='Name to greet')
+    return parser.parse_args()
 
 
 def main():
-	args = get_args()
-	print("Hello, " + args.name + "!")
+    """ run the program """
+
+    args = get_args()
+    print("Hello, " + args.name + "!")
 
 
 if __name__ == '__main__':
-	main()
+    main()
