717454d3b4c6a3e8f80ea3b8f90a768d5652869d
Mike D <mikedaug@gmail.com>
2021-03-22 11:15:43 -0500

    added argparse code. All tests passing.

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,2 +1,13 @@
+#!/usr/bin/env python3
+
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Say hello')
+parser.add_argument('-n', '--name', metavar='name', 
+					default='World', help='Name to greet')
+args = parser.parse_args()
+
 # print Hello, World to the screen
-print("Hello, World!")
+print("Hello, " + args.name + "!")
+
